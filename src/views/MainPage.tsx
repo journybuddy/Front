ea81153bfcb734fcdc9db8da2202330454933fbe
yubin012 <pyb2675@naver.com>
2024-07-20 16:58:47 +0900

    login --

---
+++ src/views/MainPage.tsx
@@ -0,0 +1,18 @@
+// src/views/Main/MainPage.tsx
+import React from 'react';
+import Header from '../components/Header';
+import Login from '../components/Login';
+
+
+const MainPage: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <Login />git 
+      <h2>Top Posts</h2>
+    </div>
+  );
+};
+
+export default MainPage;
+export {};
